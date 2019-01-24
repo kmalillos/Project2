da507c7478c2ccfae24c2078cedde6e34d1cf204
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-01-23 21:07:55 -0700

    added table information

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,65 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS colorado_db;
--- Creates the "todolist" database --
-CREATE DATABASE colorado_db;
+DROP IF EXISTS PetTracker_db;
+CREATE DATABASE PetTracker_db;
 
+CREATE TABLE PetInfo (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+pet_name VARCHAR (50) NOT NULL,
+breed VARCHAR (50) NOT NULL,
+weight DECIMAL (50) NOT NULL,
+height DECIMAL (50) NOT NULL,
+age INTEGER (50),
+primary key (id)
+);
+
+Select * from PetInfo;
+
+CREATE TABLE VetInfo (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+hospital VARCHAR (50) NOT NULL,
+vet_name VARCHAR (50) NOT NULL,
+phone_number VARCHAR (50) NOT NULL,
+address VARCHAR (50) NOT NULL,
+primary key (id)
+);
+
+Select * from VetInfo;
+
+CREATE TABLE Vaccinations (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+vaccines VARCHAR (50) NOT NULL,
+vaccine_date DATE NOT NULL,
+expires DATE NOT NULL,
+primary key (id)
+);
+
+Select * from Vaccinations;
+
+CREATE TABLE DietTracker (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+diet_type ENUM ("meal","dog treats") NOT NULL,
+given TIMESTAMP NOT NULL,
+notes VARCHAR (50) NOT NULL,
+primary key (id)
+);
+
+Select * from DietTracker;
+
+CREATE TABLE TolietTracker (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+bathroom_type ENUM ("pee","poop") NOT NULL,
+bathroom_time TIMESTAMP NOT NULL,
+notes VARCHAR (50) NOT NULL,
+primary key (id)
+);
+
+Select * from TolietTracker;
+
+CREATE TABLE Hygiene (
+id INTEGER (15) AUTO_INCREMENT NOT NULL,
+hygiene_type ENUM ("groom","nail clip", "bath") NOT NULL,
+given_when TIMESTAMP NOT NULL,
+notes VARCHAR (50) NOT NULL,
+primary key (id)
+);
+
+Select * from Hygiene;
