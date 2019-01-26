2a388954b5acaa619819de1e13d85f8136115dc4
kmalillos <malillosk@gmail.com>
2019-01-26 11:06:17 -0700

    added pet info api/routes get and post methods

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,8 @@
 // DEPENDENCIES
 // =============================================================================
 
-// // LOAD DATA HERE (EXAMPLE ONLY BELOW)
-// var activities = require("../data/activities");
+// --- access to everything sequelize. lives in index.js ---
+var db = require("../models");
 
 
 // ROUTING
@@ -11,13 +11,62 @@
 // EXPORTED TO SERVER.JS
 module.exports = function(app) {
 
-    app.get("/api/:section", function (req, res) {
-        
-        // res.json(activities);
+    // this route should find all petinfo in the table and display them as JSON
+    app.get("/api/petinfo", function(req, res) {
+        db.PetInfo.findAll({})
+        .then(function(petData) {
+            res.json(petData);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err)
+        })
     });
 
-    app.post("/api/activities", function (req, res) {
-        // console.log(req.body);
-        // res.end();
+    // this route should add a new petInfo to the table
+	app.post("/api/petinfo", function(req, res) {
+        db.PetInfo.create({
+            petName: req.body.petName,
+            breed: req.body.breed,
+            weight: req.body.weight,
+            age: req.body.age
+        })
+        .then(function(petinfoCreatedData) {
+            // .dataValues = specifies what data to console.log
+            console.log("New pet info created", petinfoCreatedData.dataValues);
+            res.json(petinfoCreatedData);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.json(err)
+        })
     });
+
+    // // to test POST in Postman
+    // {
+    //     "pet_name": "Lola",
+    //     "breed": "Airedoodle",
+    //     "weight": "26",
+    //     "age": "1"
+    // }
+
+    // // ** WORK ON DELETE LATER
+    // // this route should delete a contact from the table, if the id matches the ':id' url param
+	// app.delete("/api/petinfo/:id", function(req, res) {
+    //     db.Contact.destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     })
+    //     .then(function(petInfoDeleted) {
+    //         // .dataValues = specifies what data to console.log
+    //         console.log("Delected pet info", petInfoDeleted.dataValues)
+    //         res.json(petInfoDeleted);
+    //     })
+    //     .catch(function(err) {
+    //         console.log(err);
+    //         res.json(err)
+    //     })
+    // });
+
 };
